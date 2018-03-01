70823d84701cce5ad09fbaf68ca8c5565d418451
usama <usama@esipick.com>
2018-03-01 19:54:07 +0500

    rename filename

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\ValidatedOperands;
+use App\Http\Middleware\ValidateOperands;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -29,7 +29,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            ValidatedOperands::class,
+            ValidateOperands::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
